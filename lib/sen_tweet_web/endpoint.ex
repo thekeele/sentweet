c06030b72efe6efc1455c492e788a88fe951cbd6
Mark Keele <mark@keele.codes>
2019-05-25 20:23:39 -0600

    default to jason parsing for endpoint

--- lib/sen_tweet_web/endpoint.ex
+++ lib/sen_tweet_web/endpoint.ex
@@ -29,7 +29,7 @@ defmodule SenTweetWeb.Endpoint do
   plug Plug.Parsers,
     parsers: [:urlencoded, :multipart, :json],
     pass: ["*/*"],
-    json_decoder: Phoenix.json_library()
+    json_decoder: Jason
 
   plug Plug.MethodOverride
   plug Plug.Head
