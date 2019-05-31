9d57ad715a3dd95bf61a4f873dffe10ba5309d68
Mark Keele <mark@keele.codes>
2019-05-30 18:29:14 -0600

    use production hostname

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :sen_tweet, SenTweetWeb.Endpoint,
-  url: [host: "localhost"],
+  url: [host: "bitfeels.keele.codes"],
   cache_static_manifest: "priv/static/cache_manifest.json",
   server: true
 
