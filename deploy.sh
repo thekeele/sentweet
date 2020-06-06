7da1f85000bfa147a5160fa9d6511d0550bcd270
Mark Keele <mark@keele.codes>
2020-06-05 22:24:50 -0400

    add rebar / hex to deploy script

--- deploy.sh
+++ deploy.sh
@@ -7,8 +7,10 @@ set -e
 git pull origin master
 
 # build latest release
-MIX_ENV=prod mix deps.get --only prod
-MIX_ENV=prod mix phx.digest
+mix local.rebar --force
+mix local.hex --force
+mix deps.get --only prod
+mix phx.digest
 MIX_ENV=prod mix release --overwrite
 
 # start release as daemon with iex attached
