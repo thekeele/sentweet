d17ef479e72000c7143cfc2252fc8dcf9757d110
Mark Keele <mark@keele.codes>
2020-06-05 22:45:20 -0400

    include static assets, no node on server

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ git pull origin master
 mix local.rebar --force
 mix local.hex --force
 mix deps.get --only prod
-mix phx.digest
+MIX_ENV=prod mix phx.digest
 MIX_ENV=prod mix release --overwrite
 
 # start release as daemon with iex attached
