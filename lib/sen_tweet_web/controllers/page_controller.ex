8a0d735ef664762e341f53cf506b0d9cbd118807
rcehemann <rcehemann@gmail.com>
2020-06-09 19:38:48 -0400

    List active streams

--- lib/sen_tweet_web/controllers/page_controller.ex
+++ lib/sen_tweet_web/controllers/page_controller.ex
@@ -1,11 +1,12 @@
 defmodule SenTweetWeb.PageController do
   use SenTweetWeb, :controller
-
+  # import SenTweet.Bitfeels
   def index(conn, _params) do
     render(conn, "index.html")
   end
 
   def stream_admin(conn, _params) do
-    render(conn, "stream-admin.html", streams: [1,2,3,4,5])
+    streams = Bitfeels.all_streams()
+    render(conn, "stream-admin.html", streams: streams)
   end
 end
