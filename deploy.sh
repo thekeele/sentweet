315a5cb866cce80ef9420e80d4735d7d6d57673f
Mark Keele <mark@keele.codes>
2020-06-09 21:02:21 -0400

    check if release is running

--- deploy.sh
+++ deploy.sh
@@ -13,6 +13,18 @@ mix deps.get --only prod
 MIX_ENV=prod mix phx.digest
 MIX_ENV=prod mix release --overwrite
 
-# start release as daemon with iex attached
-_build/prod/rel/sen_tweet/bin/sen_tweet daemon_iex
+# release built, check for pid
+_build/prod/rel/sen_tweet/bin/sen_tweet pid
+
+# check if release pid is running
+# restart if pid, otherwise start release as daemon
+if [ $? -eq 0 ]
+then
+  echo "Restarting Release"
+  _build/prod/rel/sen_tweet/bin/sen_tweet restart
+else
+  echo "Starting Release as Daemon"
+  _build/prod/rel/sen_tweet/bin/sen_tweet daemon_iex
+fi
+
 echo "Release PID: $(_build/prod/rel/sen_tweet/bin/sen_tweet pid)"
