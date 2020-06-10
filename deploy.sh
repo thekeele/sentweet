45a67b4b1425800e48708fc0fc4141f16d0cefdd
Mark Keele <mark@keele.codes>
2020-06-09 21:29:20 -0400

    stop / start release

--- deploy.sh
+++ deploy.sh
@@ -13,19 +13,14 @@ mix deps.get --only prod
 MIX_ENV=prod mix phx.digest
 MIX_ENV=prod mix release --overwrite
 
-# check if release pid is running
-# restart if pid, otherwise start release as daemon
-echo "Check PID"
-_build/prod/rel/sen_tweet/bin/sen_tweet pid
-if [ $? -eq 0 ]
-then
-  echo "Restarting Release"
-  _build/prod/rel/sen_tweet/bin/sen_tweet stop
-  sleep 5s
-  _build/prod/rel/sen_tweet/bin/sen_tweet daemon_iex
-else
-  echo "Starting Release as Daemon"
-  _build/prod/rel/sen_tweet/bin/sen_tweet daemon_iex
-fi
+echo "Starting up new release"
+
+echo "Stop Release"
+_build/prod/rel/sen_tweet/bin/sen_tweet stop
+
+echo "Start Release as Daemon"
+_build/prod/rel/sen_tweet/bin/sen_tweet daemon_iex
+
+echo "Release Version: $(_build/prod/rel/sen_tweet/bin/sen_tweet version)"
 
 echo "Release PID: $(_build/prod/rel/sen_tweet/bin/sen_tweet pid)"
