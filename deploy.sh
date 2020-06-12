c8d19933ef35d8bec10b251ae105a345d1f112fe
Mark Keele <mark@keele.codes>
2020-06-11 23:22:47 -0400

    bump version

--- deploy.sh
+++ deploy.sh
@@ -1,32 +1,23 @@
 #!/bin/bash
 
-# bail if any command fails
-# set -e
+set -e
 
-# pull sentweet master
+git stash
 git pull origin master
 
-# build latest release
 mix local.rebar --force
 mix local.hex --force
 mix deps.get --only prod
 MIX_ENV=prod mix phx.digest
 MIX_ENV=prod mix release --overwrite
 
-# check if release pid is running
-# restart if pid, otherwise start release as daemon
-echo "Check PID"
-_build/prod/rel/sen_tweet/bin/sen_tweet pid
-if [ $? -eq 0 ]
-then
-  echo "Restarting Release"
-  _build/prod/rel/sen_tweet/bin/sen_tweet stop
-  sleep 3s
-  _build/prod/rel/sen_tweet/bin/sen_tweet daemon_iex
-else
-  echo "Starting Release as Daemon"
-  _build/prod/rel/sen_tweet/bin/sen_tweet daemon_iex
-fi
+echo "Restarting Release"
+_build/prod/rel/sen_tweet/bin/sen_tweet restart
 
-echo "Release Version: $(_build/prod/rel/sen_tweet/bin/sen_tweet version)"
-echo "Release PID: $(_build/prod/rel/sen_tweet/bin/sen_tweet pid)"
+echo "Release Version"
+_build/prod/rel/sen_tweet/bin/sen_tweet version
+
+echo "Release PID"
+_build/prod/rel/sen_tweet/bin/sen_tweet pid
+
+git stash pop
