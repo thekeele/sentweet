6c5b91c75e5508d4199c58bdef4d38f14db21aaa
rcehemann <rcehemann@gmail.com>
2020-06-14 17:29:56 -0400

    add new and create routes

--- lib/sen_tweet_web/controllers/stream_controller.ex
+++ lib/sen_tweet_web/controllers/stream_controller.ex
@@ -4,4 +4,13 @@ defmodule SenTweetWeb.StreamController do
   def index(conn, _params) do
     render(conn, "index.html", streams: Bitfeels.all_streams())
   end
+
+  def new(conn, _params) do
+    render(conn, "new.html")
+  end
+
+  def create(conn, %{"user" => user, "track" => track, "filter_level" => filter_level}) do
+    Bitfeels.twitter_stream(user, track, filter_level)
+    redirect(conn, to: "/admin/streams")
+  end
 end
