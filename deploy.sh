937ec3edfc8a3dab9af9074f7e230d606b356a2f
Mark Keele <mark@keele.codes>
2020-06-19 23:29:26 -0400

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -10,8 +10,12 @@ mix deps.get --only prod
 MIX_ENV=prod mix phx.digest
 MIX_ENV=prod mix release --overwrite
 
+echo "Release PID"
+_build/prod/rel/sen_tweet/bin/sen_tweet pid
+
 echo "Restarting Release"
-_build/prod/rel/sen_tweet/bin/sen_tweet restart
+_build/prod/rel/sen_tweet/bin/sen_tweet stop
+_build/prod/rel/sen_tweet/bin/sen_tweet daemon_iex
 
 echo "Release Version"
 _build/prod/rel/sen_tweet/bin/sen_tweet version
