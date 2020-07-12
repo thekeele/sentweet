ee73a6a96b95275dbd08daf7dfe57bb18d64f00b
Mark Keele <mark@keele.codes>
2020-07-12 09:54:27 -0400

    bump version to run ci

--- deploy.sh
+++ deploy.sh
@@ -10,15 +10,17 @@ mix deps.get --only prod
 MIX_ENV=prod mix phx.digest
 MIX_ENV=prod mix release --overwrite
 
-echo "Release PID"
-_build/prod/rel/sen_tweet/bin/sen_tweet pid
+_build/prod/rel/sen_tweet/bin/sen_tweet restart
 
-echo "Restarting Release"
-_build/prod/rel/sen_tweet/bin/sen_tweet stop
-_build/prod/rel/sen_tweet/bin/sen_tweet daemon_iex
+# echo "Release PID"
+# _build/prod/rel/sen_tweet/bin/sen_tweet pid
 
-echo "Release Version"
-_build/prod/rel/sen_tweet/bin/sen_tweet version
+# echo "Restarting Release"
+# _build/prod/rel/sen_tweet/bin/sen_tweet stop
+# _build/prod/rel/sen_tweet/bin/sen_tweet daemon_iex
 
-echo "Release PID"
-_build/prod/rel/sen_tweet/bin/sen_tweet pid
+# echo "Release Version"
+# _build/prod/rel/sen_tweet/bin/sen_tweet version
+
+# echo "Release PID"
+# _build/prod/rel/sen_tweet/bin/sen_tweet pid
