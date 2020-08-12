3a0a39eaec5260c1c9f51fd4e607aac5445876e1
rcehemann <rcehemann@gmail.com>
2020-08-11 20:47:49 -0400

    add weighted metrics data structure

--- lib/sen_tweet/bitfeels/metrics.ex
+++ lib/sen_tweet/bitfeels/metrics.ex
@@ -7,9 +7,19 @@ defmodule SenTweet.Bitfeels.Metrics do
   alias SenTweetWeb.MetricChannel
 
   @tweet_types ["extended_tweet", "retweeted_status", "quoted_status", "text"]
+  @tracks [:tweet_scores, :like_scores, :retweet_scores]
 
   def create_metrics do
     for type <- @tweet_types, into: %{}, do: {type, default_metrics()}
+  end
+
+  def default_metrics do
+    for track <- @tracks, into: %{}, do:
+      {track, %{count: 0, sum: 0, average: 0, histogram: init_histogram()}}
+  end
+
+  defp init_histogram do
+    Enum.map(0..10, &[-1 + 2 * &1 / 11, -1 + 2 * (&1 + 1) / 11, 0])
   end
 
   def handle_event([:bitfeels, :pipeline, :source], _measurements, metadata) do
@@ -30,42 +40,44 @@ defmodule SenTweet.Bitfeels.Metrics do
   end
 
   defp calculate_metrics(metrics, measurements, %{tweet_type: type} = metadata) do
-    %{
+    m = %{
       metrics
       | type =>
-          update_stats(measurements.score, metrics[type])
+          update_stats(measurements.score, metrics[type], metadata)
           |> Map.put(:user, metadata.user)
           |> Map.put(:track, metadata.track)
           |> Map.put(:last_metric_at, measurements.time)
     }
+    IO.puts("done!")
+    IO.inspect(m)
+    m
   end
 
-  defp update_stats(score, metrics) do
-    %{
-      tweets_processed: metrics.tweets_processed + 1,
-      sum_scores: metrics.sum_scores + score,
-      average_score: 100 * (metrics.sum_scores + score) / (metrics.tweets_processed + 1),
-      histogram: update_histogram(score, metrics.histogram)
-    }
+  defp update_stats(score, metrics, metadata) do
+    IO.inspect(metrics)
+    new_stats = %{}
+
+    for track <- @tracks do
+      weight = if(track == :tweets, do: 1, else: Map.get(metadata, track, 0))
+      count = metrics[track].count + weight
+      sum = metrics[track].sum + weight * score
+      average = if(count > 0, do: sum / count, else: 0)
+      histogram = update_histogram(score, weight, metrics[track].histogram)
+      Map.put(new_stats, track, %{count: count, sum: sum, average: average, histogram: histogram})
+    end
+    IO.inspect(new_stats)
+    new_stats
   end
 
-  defp update_histogram(score, histogram) do
+  defp update_histogram(score, weight, histogram) do
     histogram
     |> Enum.map(fn
       [left, right, count] when score >= left and score < right ->
-        [left, right, count + 1]
+        [left, right, count + weight]
 
       bin ->
         bin
     end)
   end
 
-  defp default_metrics do
-    %{
-      tweets_processed: 0,
-      sum_scores: 0,
-      average_score: 0,
-      histogram: Enum.map(0..10, &[-1 + 2 * &1 / 11, -1 + 2 * (&1 + 1) / 11, 0])
-    }
-  end
 end
