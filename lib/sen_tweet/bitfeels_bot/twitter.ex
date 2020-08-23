8f310f4dc7da73bd679c8b4afbc6b88d5bdb5a84
Mark Keele <mark@keele.codes>
2020-08-22 22:44:06 -0400

    Post images to twitter (#40)
    * add image upload and tweet_with_image for bitfeelsbot
    
    * add image size max to documentation
    
    * add image type to documentation
    
    * add deploy steps to action workflow yml

--- lib/sen_tweet/bitfeels_bot/twitter.ex
+++ lib/sen_tweet/bitfeels_bot/twitter.ex
@@ -10,39 +10,65 @@ defmodule SenTweet.BitfeelsBot.Twitter do
 
   delete_tweet - POST statuses/destroy/:id
   https://developer.twitter.com/en/docs/tweets/post-and-engage/api-reference/post-statuses-destroy-id
+
+  upload_image - POST media/upload (Image Size 5MB MAX, JPEG, JPG, PNG)
+  https://developer.twitter.com/en/docs/twitter-api/v1/media/upload-media/api-reference/post-media-upload
+  https://developer.twitter.com/en/docs/twitter-api/v1/media/upload-media/uploading-media/media-best-practices
   """
 
   alias SenTweet.BitfeelsBot.Auth
 
-  def tweet do
+  def tweet(tweet_text) do
     method = "post"
     url = "https://api.twitter.com/1.1/statuses/update.json"
-    params = [{"status", "hello twitter 🤖"}]
-    {header, req_params} = Auth.auth_header(method, url, params)
+    params = [{"status", tweet_text}]
+    {auth_header, req_params} = Auth.auth_header(method, url, params)
 
-    do_request(url, header, req_params)
+    do_request(url, [auth_header], req_params)
+  end
+
+  def tweet_with_image(tweet_text, media_id) do
+    method = "post"
+    url = "https://api.twitter.com/1.1/statuses/update.json"
+    params = [{"status", tweet_text}, {"media_ids", "#{media_id}"}]
+    {auth_header, req_params} = Auth.auth_header(method, url, params)
+
+    do_request(url, [auth_header], req_params)
   end
 
   def get_tweet(tweet_id) do
     method = "post"
     url = "https://api.twitter.com/1.1/statuses/lookup.json"
     params = [{"id", tweet_id}]
-    {header, req_params} = Auth.auth_header(method, url, params)
+    {auth_header, req_params} = Auth.auth_header(method, url, params)
 
-    do_request(url, header, req_params)
+    do_request(url, [auth_header], req_params)
   end
 
   def delete_tweet(tweet_id) do
     method = "post"
     url = "https://api.twitter.com/1.1/statuses/destroy/#{tweet_id}.json"
-    params = []
-    {header, req_params} = Auth.auth_header(method, url, params)
+    {auth_header, req_params} = Auth.auth_header(method, url, [])
 
-    do_request(url, header, req_params)
+    do_request(url, [auth_header], req_params)
   end
 
-  defp do_request(url, header, req_params) do
-    case :hackney.post(url, [header], {:form, req_params}, [:with_body]) do
+  def upload_image(image_name) do
+    method = "post"
+    url = "https://upload.twitter.com/1.1/media/upload.json"
+
+    image_path = "images/" <> image_name
+    encoded_image = image_path |> File.read!() |> Base.encode64()
+
+    params = [{"media_data", encoded_image}]
+    {auth_header, req_params} = Auth.auth_header(method, url, params)
+    headers = [auth_header, {"Content-Type", "application/x-www-form-urlencoded"}]
+
+    do_request(url, headers, req_params)
+  end
+
+  defp do_request(url, headers, req_params) do
+    case :hackney.post(url, headers, {:form, req_params}, [:with_body]) do
       {:ok, _status, _headers, body} ->
         {:ok, Jason.decode!(body)}
 
