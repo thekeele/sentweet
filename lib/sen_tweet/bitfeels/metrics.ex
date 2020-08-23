8f204c9b05ae469f91b9a455e484a8eedc91f6f2
rcehemann <rcehemann@gmail.com>
2020-08-23 16:16:03 -0400

    Move calculations to SenTweet.Bitfeels.Stats

--- lib/sen_tweet/bitfeels/metrics.ex
+++ lib/sen_tweet/bitfeels/metrics.ex
@@ -4,22 +4,11 @@ defmodule SenTweet.Bitfeels.Metrics do
   """
 
   alias SenTweet.Bitfeels.MetricServer
+  alias SenTweet.Bitfeels.Stats
   alias SenTweetWeb.MetricChannel
 
-  @tweet_types ["extended_tweet", "retweeted_status", "quoted_status", "text"]
-  @tracks [:tweet_scores, :like_scores, :retweet_scores]
-
   def create_metrics do
-    for type <- @tweet_types, into: %{}, do: {type, default_metrics()}
-  end
-
-  def default_metrics do
-    for track <- @tracks, into: %{}, do:
-      {track, %{count: 0, sum: 0, average: 0, histogram: init_histogram()}}
-  end
-
-  defp init_histogram do
-    Enum.map(0..10, &[-1 + 2 * &1 / 11, -1 + 2 * (&1 + 1) / 11, 0])
+    Stats.create_stats()
   end
 
   def handle_event([:bitfeels, :pipeline, :source], _measurements, metadata) do
@@ -30,7 +19,9 @@ defmodule SenTweet.Bitfeels.Metrics do
       when is_float(score) do
     metadata
     |> MetricServer.get_metrics()
-    |> calculate_metrics(measurements, metadata)
+    |> IO.inspect(label: "input metrics")
+    |> Stats.update_all_stats(measurements, metadata)
+    |> IO.inspect(label: "calculated metrics")
     |> MetricServer.update_metrics(metadata)
     |> MetricChannel.broadcast_metrics()
   end
@@ -38,46 +29,4 @@ defmodule SenTweet.Bitfeels.Metrics do
   def handle_event(_event, _measurements, _metadata) do
     %{}
   end
-
-  defp calculate_metrics(metrics, measurements, %{tweet_type: type} = metadata) do
-    m = %{
-      metrics
-      | type =>
-          update_stats(measurements.score, metrics[type], metadata)
-          |> Map.put(:user, metadata.user)
-          |> Map.put(:track, metadata.track)
-          |> Map.put(:last_metric_at, measurements.time)
-    }
-    IO.puts("done!")
-    IO.inspect(m)
-    m
-  end
-
-  defp update_stats(score, metrics, metadata) do
-    IO.inspect(metrics)
-    new_stats = %{}
-
-    for track <- @tracks do
-      weight = if(track == :tweets, do: 1, else: Map.get(metadata, track, 0))
-      count = metrics[track].count + weight
-      sum = metrics[track].sum + weight * score
-      average = if(count > 0, do: sum / count, else: 0)
-      histogram = update_histogram(score, weight, metrics[track].histogram)
-      Map.put(new_stats, track, %{count: count, sum: sum, average: average, histogram: histogram})
-    end
-    IO.inspect(new_stats)
-    new_stats
-  end
-
-  defp update_histogram(score, weight, histogram) do
-    histogram
-    |> Enum.map(fn
-      [left, right, count] when score >= left and score < right ->
-        [left, right, count + weight]
-
-      bin ->
-        bin
-    end)
-  end
-
 end
---
+++ lib/sen_tweet/bitfeels/stats.ex
@@ -0,0 +1,65 @@
+defmodule SenTweet.Bitfeels.Stats do
+
+  @tweet_types [:extended_tweet, :retweeted_status, :quoted_status, :text]
+  @weight_factors [:tweets, :likes, :retweets]
+
+  def create_stats do
+    created = for type <- @tweet_types, into: %{}, do: {type, empty_stats()}
+    IO.inspect(created, label: "Created Metrics")
+  end
+
+  def update_all_stats(all_stats, measurements, %{tweet_type: type} = metadata) do
+    type = String.to_atom(type)
+    %{
+      all_stats
+      | type =>
+          update_type_stats(all_stats[type], measurements.score, metadata)
+          |> IO.inspect(label: "updated stats")
+          |> Map.put(:user, metadata.user)
+          |> Map.put(:track, metadata.track)
+          |> Map.put(:last_metric_at, measurements.time)
+    }
+  end
+
+  defp update_type_stats(type_stats, score, metadata) do
+    Enum.reduce(@weight_factors, %{}, fn factor, new_type_stats ->
+      IO.inspect({factor, new_type_stats}, label: "factor and stats")
+      Map.put(new_type_stats, factor,
+              update_weighted_stats(type_stats[factor], score, Map.get(metadata, factor, 1)))
+    end)
+  end
+
+  defp update_weighted_stats(stats, score, weight) when weight > 0 do
+    %{
+      count: stats.count + weight,
+      sum: stats.sum + weight * score,
+      average: (stats.sum + weight) / (stats.count + weight),
+      histogram: update_histogram(stats.histogram, score, weight)
+    }
+  end
+
+  defp update_weighted_stats(stats, _score, weight) when weight == 0 do
+    stats
+  end
+
+  defp update_histogram(histogram, score, weight) do
+    histogram
+    |> Enum.map(fn
+      [left, right, count] when score >= left and score < right ->
+        [left, right, count + weight]
+
+      bin ->
+        bin
+    end)
+  end
+
+  defp empty_stats do
+    for factor <- @weight_factors, into: %{}, do:
+      {factor, %{count: 0, sum: 0, average: 0, histogram: empty_histogram()}}
+  end
+
+  defp empty_histogram do
+    Enum.map(0..10, &[-1 + 2 * &1 / 11, -1 + 2 * (&1 + 1) / 11, 0])
+    |> IO.inspect(label: "Histogram")
+  end
+end
