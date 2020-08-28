a585cdddfa33d0ae9916807945cc237aab039c62
Mark Keele <mark@keele.codes>
2020-08-27 22:22:26 -0400

    add hourly stats server, stores stats in state by current hour

---
+++ lib/sen_tweet/bitfeels/events.ex
@@ -0,0 +1,17 @@
+defmodule SenTweet.Bitfeels.Events do
+
+  alias SenTweet.Bitfeels.{Stats, HourlyStats}
+
+  def handle_event([:bitfeels, :pipeline, :sentiment], %{score: score} = measurements, metadata)
+      when is_float(score) do
+    metadata
+    |> HourlyStats.get()
+    |> Stats.update_score(measurements, metadata)
+    |> Stats.update_metadata(measurements, metadata)
+    |> HourlyStats.put(metadata)
+  end
+
+  def handle_event(_event, _measurements, _metadata) do
+    %{}
+  end
+end
---
+++ lib/sen_tweet/bitfeels/hourly_stats.ex
@@ -0,0 +1,74 @@
+defmodule SenTweet.Bitfeels.HourlyStats do
+  @moduledoc """
+  Process that manages hourly statistics for bitfeels
+  """
+  use GenServer
+
+  alias SenTweet.Bitfeels.Stats
+
+  # Client
+
+  def start_link(opts) do
+    GenServer.start_link(__MODULE__, opts, name: __MODULE__)
+  end
+
+  def all() do
+    GenServer.call(__MODULE__, :all)
+  end
+
+  def get(metadata) do
+    GenServer.call(__MODULE__, {:get, metadata})
+  end
+
+  def put(stats, metadata) do
+    GenServer.call(__MODULE__, {:put, stats, metadata})
+  end
+
+  # Server
+
+  def init(_opts) do
+    {:ok, %{}}
+  end
+
+  def handle_call(:all, _from, state) do
+    {:reply, state, state}
+  end
+
+  def handle_call({:get, metadata}, _from, state) do
+    stream_key = stream_key(metadata)
+    current_hour = current_hour()
+    hourly_stats = get_hourly_stats(state[stream_key], current_hour)
+
+    {:reply, hourly_stats[current_hour], %{stream_key => hourly_stats}}
+  end
+
+  def handle_call({:put, stats, metadata}, _from, state) do
+    stream_key = stream_key(metadata)
+    current_hour = current_hour()
+    hourly_stats =
+      state[stream_key] |> get_hourly_stats(current_hour) |> Map.put(current_hour, stats)
+
+    {:reply, hourly_stats[current_hour], %{stream_key => hourly_stats}}
+  end
+
+  # Helper functions
+
+  defp stream_key(%{user: user, track: track}) do
+    "#{user}_#{track}"
+  end
+
+  defp current_hour() do
+    time = Time.utc_now()
+    time.hour
+  end
+
+  defp get_hourly_stats(nil, current_hour), do: %{current_hour => Stats.create()}
+
+  defp get_hourly_stats(hourly_stats, current_hour) do
+    case hourly_stats do
+      %{^current_hour => _} -> hourly_stats
+
+      _new_hour -> Map.put(hourly_stats, current_hour, Stats.create())
+    end
+  end
+end
