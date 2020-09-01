afa874223de03033ce3d9527116f39682357c737
Mark Keele <mark@keele.codes>
2020-09-01 17:25:45 -0400

    Aggregate Stats (#47)
    * add Stats.aggregate fun, still need to sum histograms
    
    * add fun to aggregate histograms
    
    * bump version

--- lib/sen_tweet/bitfeels/stats.ex
+++ lib/sen_tweet/bitfeels/stats.ex
@@ -6,21 +6,30 @@ defmodule SenTweet.Bitfeels.Stats do
   @tweet_types [:extended_tweet, :retweeted_status, :quoted_status, :text]
   @weight_factors [:tweets, :likes, :retweets]
 
+  @doc """
+  Create an empty stats structure
+  """
   def create do
     for type <- @tweet_types, into: %{}, do: {type, empty_stats()}
   end
 
+  defp empty_stats do
+    for factor <- @weight_factors,
+        into: %{},
+        do: {factor, %{count: 0, sum: 0, average: 0, histogram: empty_histogram()}}
+  end
+
+  defp empty_histogram do
+    Enum.map(0..10, &[-1 + 2 * &1 / 11, -1 + 2 * (&1 + 1) / 11, 0])
+  end
+
+  @doc """
+  Given existing stats and a new set of measurements, update the score
+  """
   def update_score(stats, measurements, %{tweet_type: type} = metadata) do
     type = String.to_existing_atom(type)
 
     %{stats | type => update_type_stats(stats[type], measurements.score, metadata)}
-  end
-
-  def update_metadata(stats, measurements, metadata) do
-    stats
-    |> Map.put(:user, metadata.user)
-    |> Map.put(:track, metadata.track)
-    |> Map.put(:last_metric_at, measurements.time)
   end
 
   defp update_type_stats(type_stats, score, metadata) do
@@ -57,13 +66,67 @@ defmodule SenTweet.Bitfeels.Stats do
     end)
   end
 
-  defp empty_stats do
-    for factor <- @weight_factors,
-        into: %{},
-        do: {factor, %{count: 0, sum: 0, average: 0, histogram: empty_histogram()}}
+  @doc """
+  Given existing stats and a new set of measurements, update non-score data
+  """
+  def update_metadata(stats, measurements, metadata) do
+    stats
+    |> Map.put(:user, metadata.user)
+    |> Map.put(:track, metadata.track)
+    |> Map.put(:last_metric_at, measurements.time)
   end
 
-  defp empty_histogram do
-    Enum.map(0..10, &[-1 + 2 * &1 / 11, -1 + 2 * (&1 + 1) / 11, 0])
+  @doc """
+  Given a list of stats, aggregate the data into one stat
+
+  Examples
+
+      iex> hourly_stats = for hour <- 0..23, into: %{}, do: {hour, SenTweet.Bitfeels.Stats.create()}
+      %{
+      ...
+      }
+
+      iex> SenTweet.Bitfeels.Stats.aggregate(hourly_stats)
+      %{
+      ...
+      }
+  """
+  def aggregate(hourly_stats) do
+    hourly_stats
+    |> Map.values()
+    |> Enum.reduce(%{}, &aggregate_data/2)
+  end
+
+  defp aggregate_data(existing_stats, aggregated_stats) do
+    Map.merge(aggregated_stats, existing_stats, &aggregate_types/3)
+  end
+
+  defp aggregate_types(_type, left, right) do
+    Map.merge(left, right, &aggregate_weights/3)
+  end
+
+  defp aggregate_weights(_weight, left, right) do
+    aggregated_count = left.count + right.count
+    aggregated_sum = left.sum + right.sum
+    aggregated_average = 100 * (aggregated_sum / aggregated_count)
+    aggregated_histogram = aggregate_histograms(left.histogram, right.histogram)
+
+    %{
+      count: aggregated_count,
+      sum: aggregated_sum,
+      average: aggregated_average,
+      histogram: aggregated_histogram
+    }
+  end
+
+  defp aggregate_histograms([], []), do: []
+
+  defp aggregate_histograms(
+         [[lower_bound, upper_bound, left_count] | rest_left],
+         [[_, _, right_count] | rest_right]
+       ) do
+    bin = [lower_bound, upper_bound, left_count + right_count]
+
+    [bin | aggregate_histograms(rest_left, rest_right)]
   end
 end
