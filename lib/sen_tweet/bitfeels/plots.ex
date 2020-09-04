3b9f2f23750efc0b17277ed116503b591b8dfd33
Chris Ehemann <rcehemann@gmail.com>
2020-09-04 16:20:21 -0400

    placeholder plot

---
+++ lib/sen_tweet/bitfeels/plots.ex
@@ -0,0 +1,13 @@
+defmodule SenTweet.Bitfeels.Plots do
+
+  alias SenTweet.Bitfeels.Stats
+
+  def create(stats \\ Stats.create()) do
+    hist = stats[:text][:likes][:histogram]
+    data = Enum.map(hist, fn [l, r, c] -> [(l+r)/2, c] end)
+    dataset = Contex.Dataset.new(data)
+    plot_content = Contex.BarChart.new(dataset)
+    plot = Contex.Plot.new(600, 400, plot_content)
+    Contex.Plot.to_svg(plot)
+  end
+end
