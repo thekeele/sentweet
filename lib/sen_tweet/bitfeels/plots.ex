9344fdce3551b7be45d01ffa8c23ac0c260b7071
Mark Keele <mark@keele.codes>
2020-09-04 17:09:42 -0400

    encode safe string

--- lib/sen_tweet/bitfeels/plots.ex
+++ lib/sen_tweet/bitfeels/plots.ex
@@ -1,13 +1,14 @@
 defmodule SenTweet.Bitfeels.Plots do
-
   alias SenTweet.Bitfeels.Stats
 
   def create(stats \\ Stats.create()) do
     hist = stats[:text][:likes][:histogram]
-    data = Enum.map(hist, fn [l, r, c] -> [(l+r)/2, c] end)
+    data = Enum.map(hist, fn [l, r, c] -> [(l + r) / 2, c] end)
     dataset = Contex.Dataset.new(data)
     plot_content = Contex.BarChart.new(dataset)
     plot = Contex.Plot.new(600, 400, plot_content)
-    Contex.Plot.to_svg(plot)
+    svg = Contex.Plot.to_svg(plot)
+    string = Phoenix.HTML.safe_to_string(svg)
+    Base.encode64(string, limit: :infinity)
   end
 end
