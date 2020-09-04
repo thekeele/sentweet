a23579cb02234440f600126cce29c2d362055755
Chris Ehemann <rcehemann@gmail.com>
2020-09-04 17:49:38 -0400

    plot real data

--- lib/sen_tweet/bitfeels/plots.ex
+++ lib/sen_tweet/bitfeels/plots.ex
@@ -1,12 +1,30 @@
 defmodule SenTweet.Bitfeels.Plots do
   alias SenTweet.Bitfeels.Stats
+  alias SenTweet.Bitfeels.Stats.Data
 
-  def create(stats \\ Stats.create()) do
-    hist = stats[:text][:likes][:histogram]
-    data = Enum.map(hist, fn [l, r, c] -> [(l + r) / 2, c] end)
-    dataset = Contex.Dataset.new(data)
-    plot_content = Contex.BarChart.new(dataset)
-    plot = Contex.Plot.new(600, 400, plot_content)
-    svg = Contex.Plot.to_svg(plot)
+  def create(stats \\ Data.hourly_stats(), params \\ %{}) do
+    agg_histogram(stats, params)
+    |> merge_bin_edges()
+    |> Contex.Dataset.new()
+    |> Contex.BarChart.new()
+    |> create_canvas()
+    |> Contex.Plot.to_svg()
   end
+
+  defp agg_histogram(stats, %{tweet_type: tweet_type, weight: weight}) do
+    Stats.aggregate(stats)[tweet_type][weight][:histogram]
+  end
+
+  defp agg_histogram(stats, _params) do
+    Stats.aggregate(stats)[:extended_tweet][:tweets][:histogram]
+  end
+
+  defp merge_bin_edges(histogram) do
+    Enum.map(histogram, fn [left, right, count] -> [(left + right) / 2, count] end)
+  end
+
+  defp create_canvas(plot_content) do
+    Contex.Plot.new(600, 400, plot_content)
+  end
+
 end
