d1062c4005e079e7de7a09b2ace0f0748e2bee20
Mark Keele <mark@keele.codes>
2020-09-04 19:51:05 -0400

    subscribe and broadcast stats updates

--- lib/sen_tweet/bitfeels/events.ex
+++ lib/sen_tweet/bitfeels/events.ex
@@ -4,15 +4,24 @@ defmodule SenTweet.Bitfeels.Events do
   """
   alias SenTweet.Bitfeels.{HourlyStats, Stats}
 
+  @topic "SenTweetWeb.PageLive"
+
   def handle_event([:bitfeels, :pipeline, :sentiment], %{score: score} = measurements, metadata)
       when is_float(score) do
     metadata
     |> HourlyStats.get()
     |> Stats.update_score(measurements, metadata)
     |> HourlyStats.put(metadata)
+    |> broadcast_stats()
   end
 
   def handle_event(_event, _measurements, _metadata) do
     %{}
+  end
+
+  defp broadcast_stats(hourly_stats) do
+    message = {:bitfeels, hourly_stats}
+
+    Phoenix.PubSub.broadcast(SenTweet.PubSub, @topic, message)
   end
 end
--- lib/sen_tweet_web/live/page_live.ex
+++ lib/sen_tweet_web/live/page_live.ex
@@ -5,15 +5,20 @@ defmodule SenTweetWeb.PageLive do
 
   alias SenTweet.Bitfeels.Plots
 
+  @topic inspect(__MODULE__)
+
   @impl true
   def mount(params, session, socket) do
     IO.inspect(params, label: "params")
     IO.inspect(session, label: "session")
     IO.inspect(socket, label: "socket")
+    IO.inspect(connected?(socket), label: "connected")
+
+    if connected?(socket), do: Phoenix.PubSub.subscribe(SenTweet.PubSub, @topic) |> IO.inspect(label: "subscribed")
 
     svg = Plots.create()
 
-    {:ok, assign(socket, svg: svg) |> IO.inspect(label: "assign")}
+    {:ok, assign(socket, svg: svg)}
   end
 
   @impl true
@@ -21,5 +26,15 @@ defmodule SenTweetWeb.PageLive do
     IO.inspect(event, label: "event")
     IO.inspect(params, label: "params")
     IO.inspect(socket, label: "socket")
+  end
+
+  @impl true
+  def handle_info({:bitfeels, stats}, socket) do
+    IO.puts "bitfeels stats"
+    IO.inspect(socket, label: "socket")
+
+    svg = Plots.create(stats)
+
+    {:noreply, assign(socket, svg: svg)}
   end
 end
