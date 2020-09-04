64018493321a5b8c441181a2c5f4f3bdc1b2e544
Chris Ehemann <rcehemann@gmail.com>
2020-09-04 19:56:08 -0400

    sentiment plot with labels

--- lib/sen_tweet/bitfeels/plots.ex
+++ lib/sen_tweet/bitfeels/plots.ex
@@ -1,14 +1,23 @@
 defmodule SenTweet.Bitfeels.Plots do
   alias SenTweet.Bitfeels.Stats
   alias SenTweet.Bitfeels.Stats.Data
+  alias Contex.{Dataset, BarChart, Plot}
+
+  @colors ["63595c"]
+  @tick_precision 2
 
   def create(stats \\ Data.hourly_stats(), params \\ %{}) do
+
     agg_histogram(stats, params)
     |> merge_bin_edges()
-    |> Contex.Dataset.new()
-    |> Contex.BarChart.new()
+    |> Dataset.new()
+    |> BarChart.new()
+    |> BarChart.colours(@colors)
+    |> BarChart.data_labels(false)
+    |> BarChart.padding(4)
     |> create_canvas()
-    |> Contex.Plot.to_svg()
+    |> Plot.axis_labels("Sentiment", "Frequency")
+    |> Plot.to_svg()
   end
 
   defp agg_histogram(stats, %{tweet_type: tweet_type, weight: weight}) do
@@ -19,12 +28,26 @@ defmodule SenTweet.Bitfeels.Plots do
     Stats.aggregate(stats)[:extended_tweet][:tweets][:histogram]
   end
 
+  defp format_edge(float) do
+    float
+    |> Float.round(@tick_precision)
+    |> Float.to_string()
+  end
+
+  defp format_tick_label(left, right) do
+    Enum.map([left, right], &format_edge/1)
+    |> Enum.join(" to ")
+  end
+
   defp merge_bin_edges(histogram) do
-    Enum.map(histogram, fn [left, right, count] -> [(left + right) / 2, count] end)
+    Enum.map(histogram, fn [left, right, count] ->
+      [format_tick_label(left, right), count]
+    end)
+    |> IO.inspect()
   end
 
   defp create_canvas(plot_content) do
-    Contex.Plot.new(600, 400, plot_content)
+    Plot.new(600, 400, plot_content)
   end
 
 end
