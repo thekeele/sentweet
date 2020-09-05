71086ca5984e9583420d50d14ff1ce28bb64222d
Mark Keele <mark@keele.codes>
2020-09-04 21:17:58 -0400

    plots create accepts a histogram

--- lib/sen_tweet/bitfeels/plots.ex
+++ lib/sen_tweet/bitfeels/plots.ex
@@ -1,14 +1,11 @@
 defmodule SenTweet.Bitfeels.Plots do
-  alias SenTweet.Bitfeels.Stats
-  alias SenTweet.Bitfeels.Stats.Data
   alias Contex.{Dataset, BarChart, Plot}
 
   @colors ["63595c"]
   @tick_precision 2
 
-  def create(stats \\ Data.hourly_stats(), params \\ %{}) do
-
-    agg_histogram(stats, params)
+  def create(histogram) do
+    histogram
     |> merge_bin_edges()
     |> Dataset.new()
     |> BarChart.new()
@@ -18,14 +15,6 @@ defmodule SenTweet.Bitfeels.Plots do
     |> create_canvas()
     |> Plot.axis_labels("Sentiment", "Frequency")
     |> Plot.to_svg()
-  end
-
-  defp agg_histogram(stats, %{tweet_type: tweet_type, weight: weight}) do
-    Stats.aggregate(stats)[tweet_type][weight][:histogram]
-  end
-
-  defp agg_histogram(stats, _params) do
-    Stats.aggregate(stats)[:extended_tweet][:tweets][:histogram]
   end
 
   defp format_edge(float) do
@@ -43,11 +32,9 @@ defmodule SenTweet.Bitfeels.Plots do
     Enum.map(histogram, fn [left, right, count] ->
       [format_tick_label(left, right), count]
     end)
-    |> IO.inspect()
   end
 
   defp create_canvas(plot_content) do
     Plot.new(600, 400, plot_content)
   end
-
 end
